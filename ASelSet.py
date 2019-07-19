ea3cb972fe7927d10e84b7a92a877c6d9b3f4e5b
Yurii Andropov <yuriy.andropov@live.com>
2019-07-19 19:25:06 -0400

    set and start of recall

--- ASelSet.py
+++ ASelSet.py
@@ -32,14 +32,10 @@ bl_info = {
 
 import bpy
 
-
-
-
 class ASel_Set(bpy.types.Operator):
     bl_idname = "object.asel_set"
     bl_label = "ASelSet Set"
     bl_options = {'REGISTER', 'UNDO'}
-
 
     def updateSet(self,context):
         if self.bSet:
@@ -52,6 +48,7 @@ class ASel_Set(bpy.types.Operator):
             self.bSet = False
             self.bSub = False
         return None
+
     def updateSub(self,context):
         if self.bSub:
             self.bAdd = False
@@ -115,36 +112,102 @@ class ASel_Set(bpy.types.Operator):
                 if item[0]==self.objSets:
                     name = item[1]
         for obj in bpy.context.selected_objects:
+            obj.update_from_editmode()
             #get selected vert ID
             verts = []
             allVerts = []
             data = obj.data
-            if self.objSets == '0':
-                if len(obj.vertex_groups)>0:
-                #checking group name
-                    for group in obj.vertex_groups:
-                        if group.name == name:
-                            vGroup = group
-                            break
-                    vGroup = obj.vertex_groups.new(name=self.newSet)
-                else:
-                    vGroup = obj.vertex_groups.new(name=self.newSet)
+            vGroup = None
             for vert in data.vertices:
                 allVerts.append(vert.index)
                 if vert.select:
                     verts.append(vert.index)
+            for group in obj.vertex_groups:
+                if group.name == name:
+                    vGroup = group
+                    break
+            if vGroup == None:
+                vGroup = obj.vertex_groups.new(name=self.newSet)
             #adding to group is possible only in object mode
             bpy.ops.object.mode_set(mode='OBJECT', toggle=False)
             if self.bSet:
                 vGroup.remove(allVerts)
                 vGroup.add(verts,1,'REPLACE')
             elif self.bAdd:
-                vGroup.add(verts,1,'REPLACE')
+                vGroup.add(verts,1,'ADD')
             elif self.bSub:
                 vGroup.remove(verts)
             bpy.ops.object.mode_set(mode='EDIT', toggle=False)
         return {'FINISHED'}
 
+class ASel_Get(bpy.types.Operator):
+    bl_idname = "object.asel_get"
+    bl_label = "ASelSet Get"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def updateGet(self,context):
+        if self.bGet:
+            self.bAdd = False
+            self.bSub = False
+        return None
+    
+    def updateAdd(self,context):
+        if self.bAdd :
+            self.bSet = False
+            self.bSub = False
+        return None
+
+    def updateSub(self,context):
+        if self.bSub:
+            self.bAdd = False
+            self.bSet = False
+        return None
+    
+    def updateList(self,context):
+        sets = []
+        id = 1
+        for obj in bpy.context.selected_objects:
+            for group in obj.vertex_groups:
+                if group.name != None:
+                    name = group.name
+                    icon = 'GROUP_VERTEX'
+                    if (str(id),name,name,icon,id) not in sets:
+                        sets.append((str(id),name,name,icon,id))
+                        id+=1
+        return sets
+
+    bGet: bpy.props.BoolProperty(default=True,name='Get',update=updateGet)
+    bAdd: bpy.props.BoolProperty(default=False,name='Add',update=updateAdd)
+    bSub: bpy.props.BoolProperty(default=False,name='Sub',update=updateSub)
+    objSets:bpy.props.EnumProperty(name="Objects Sets",items=updateList)
+
+    def draw(self,context):
+        layout = self.layout
+        setBox = layout.box()
+        sRow = setBox.row(align=True)
+        nRow = setBox.row(align=True)
+        bRow = setBox.row(align=True)
+
+        sRow.prop(self,"objSets")
+        nRow.prop(self,'newSet')
+        bRow.prop(self,"bGet")
+        bRow.prop(self,"bAdd")
+        bRow.prop(self,"bSub")
+
+    def invoke(self,context,event):
+        if bpy.context.mode != "EDIT_MESH":
+            self.report({'WARNING'},'Invalid mode')
+            return{'CANCELLED'}
+        return context.window_manager.invoke_props_dialog(self, width = 200)
+    
+    def execute(self,context):
+        sets = self.updateList(context)
+        for item in sets:
+            if item[0]==self.objSets:
+                name = item[1]
+        for obj in bpy.context.selected_objects:
+            data = obj.data
+        return {'FINISHED'}
     
 
 
