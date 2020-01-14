1fe6244661bd80a4c5395a696bd78a6cd66e05ca
Yurii Andropov <yuriy.andropov@live.com>
2020-01-13 19:49:29 -0500

    Revert "Merge branch 'master' of https://github.com/YuriyAndropov/blenderPython"
    This reverts commit 647c8e8d2abcb20f8b0c0de3f3064c1484633144, reversing
    changes made to fdae09d68be558de0f26a9dec7351d8d711a03fa.

---
+++ bAPISnippets.py
@@ -0,0 +1,40 @@
+#just a file for bpy snippets
+
+#defs are for quick vs navigation
+def Collections():
+    #access all collections in the scene
+    bpy.data.collections
+    #create new collection
+    newCol = bpy.data.collections.new("Name")
+    #link collection to scene
+    bpy.context.scene.collection.children.link(newCol)
+    #link object to collection
+    newCol.objects.link(newObj)
+
+def Object():
+    #create new object
+    newObj = bpy.data.objects.new("Name", meshData)
+    #delete object
+    bpy.data.objects.remove(object,do_unlink=True,do_id_user=True,do_ui_user=True)
+    #link object to the scene(to Master Collection)
+    bpy.context.collection.objects.link(newObj)
+    #select an object
+    object.select_set(True) 
+    #make object active
+    bpy.context.view_layer.objects.active = object
+    #parent one object1 to object2
+    object1.parent = object2
+    #access to children
+    object.children
+    #object custom properties
+    bpy.ops.wm.properties_add(data_path="object.data")
+
+def Context():
+    #set context mode
+    #'OBJECT', 'EDIT', 'SCULPT', 'VERTEX_PAINT', 'WEIGHT_PAINT', 'TEXTURE_PAINT', 'PARTICLE_EDIT', 'POSE'
+    bpy.ops.object.mode_set(mode='EDIT', toggle=False)
+
+def Viewport():
+    #redraw viewport
+    context.area.tag_redraw()
+    
